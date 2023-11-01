3a1de9ec4d763c7361949e241b9e85f0a4c58cf1
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2023-11-01 13:17:20 +0100

    Pimped login page

--- pages/auth/signIn/index.js
+++ pages/auth/signIn/index.js
@@ -5,21 +5,61 @@ import React from 'react'
 export default function LoginPage({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <p key={provider.name}>
-          <a href={provider.signinUrl} onClick={(e) => e.preventDefault()}>
-            <button
-              onClick={() =>
-                signIn(provider.id, {
-                  callbackUrl: '',
-                })
-              }
-            >
-              Sign in with {provider.name}
-            </button>
-          </a>
-        </p>
-      ))}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Alten Deks
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              {Object.values(providers).map((provider) => (
+                <p key={provider.name}>
+                  <a
+                    href={provider.signinUrl}
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <button
+                      onClick={() =>
+                        signIn(provider.id, {
+                          callbackUrl: '',
+                        })
+                      }
+                      className={`flex 
+                                  w-full 
+                                  justify-center 
+                                  rounded-md 
+                                  bg-indigo-600 
+                                  px-3 
+                                  py-5 
+                                  font-semibold 
+                                  leading-6 
+                                  text-white 
+                                  shadow-sm 
+                                  hover:bg-indigo-500 
+                                  focus-visible:outline 
+                                  focus-visible:outline-2 
+                                  focus-visible:outline-offset-2 
+                                  focus-visible:outline-indigo-600`}
+                    >
+                      Sign in with {provider.name}
+                    </button>
+                  </a>
+                </p>
+              ))}
+            </div>
+
+            <div></div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Alten Decks is a company-scoped online community training
+            applicaiton.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
